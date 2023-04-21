939578459ad1cd417415413826d170c70409b985
Alex <alex.valle@gmail.com>
2023-04-20 21:57:10 -0400

    breaking down menu dashboard

---
+++ src/renderer/src/components/menu/MenuCategoriesGrid.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid grid-cols-2 md:grid-cols-4 gap-2">
+    <button
+      v-for="category in categories"
+      :key="category.id"
+      type="button"
+      class="bg-pink-100 text-base-bg rounded p-4 flex flex-col h-28 justify-between"
+      @click="selectedCategory = category.id"
+    >
+      <div><AppIcon name="pizza" class="h-6 w-6" /></div>
+      <div class="text-left">
+        <h4 class="font-semibold">{{ category.name }}</h4>
+        <p class="text-xs text-base-text">{{ category.items?.length }} items</p>
+      </div>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MENU_KEY } from '@renderer/keys'
+import { requireInjection } from '@renderer/utils'
+import AppIcon from '../AppIcon.vue'
+
+const { categories, selectedCategory } = requireInjection(MENU_KEY)
+</script>
